be5e0827b18c0144a1a27cef95eb71884bd51ce5
Irvin Fiz <irvin.fiz.dev@gmail.com>
2020-09-28 15:09:33 -0500

    graph false

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) =>{
   });
 
   app.use('/graph', graphqlHTTP({
-    graphiql: true,
+    graphiql: false,
     schema: schema,
     rootValue: resolvers,
     customFormatErrorFn(err) {
